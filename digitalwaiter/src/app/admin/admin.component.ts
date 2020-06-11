6ec8affa274cf0e37b6cea5170a0d712eef53457
dbohdiev <dbohdiev@vmware.com>
2020-06-11 19:02:12 +0300

    acyive link

--- digitalwaiter/src/app/admin/admin.component.ts
+++ digitalwaiter/src/app/admin/admin.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Link} from "../header/header.component";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+    selector: 'app-admin',
+    templateUrl: './admin.component.html',
+    styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
 
-  public links: Link[] = [
-    {
-      name: "Admin Tool",
-      path: "general"
-    },
-    {
-      name: "Queue",
-      path: "queue"
+    public links: Link[] = [
+        {
+            name: "Admin Tool",
+            path: "general",
+        },
+        {
+            name: "Queue",
+            path: "queue"
+        }
+    ]
+
+    constructor() {
     }
-  ]
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
--- digitalwaiter/src/app/header/header.component.ts
+++ digitalwaiter/src/app/header/header.component.ts
@@ -1,23 +1,42 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 export interface Link {
-  name: string;
-  path: string;
+    name: string;
+    path: string;
+    active?: boolean;
 }
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    private lastLink: Link;
+    private _links: Link[];
 
-  @Input()
-  links: Link[];
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    @Input()
+    set links(links: Link[]) {
+        this._links = links;
+        this.lastLink = links[0];
+        this.lastLink.active = true;
+    }
+
+    get links(): Link[] {
+        return this._links;
+    }
+
+    ngOnInit(): void {
+    }
+
+    clickOnLink(link: Link) {
+        link.active = true;
+        this.lastLink.active = false;
+        this.lastLink = link;
+    }
 
 }
