fac8a5c6a0a41f83d726579916fb7c6b1bf0ce41
dbohdiev <dbohdiev@vmware.com>
2020-06-12 20:08:54 +0300

    WIP: Save modal

---
+++ digitalwaiter/src/app/save-modal/save-modal.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SaveModalComponent } from './save-modal.component';
+
+
+
+@NgModule({
+  declarations: [SaveModalComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SaveModalModule { }
