7a551c8a729f46b14fc3be9ea93b641c12ec01bf
dbohdiev <dbohdiev@vmware.com>
2020-06-12 16:38:19 +0300

    fixData100500

--- digitalwaiter/src/app/admin/admin-tool/admin-tool.component.ts
+++ digitalwaiter/src/app/admin/admin-tool/admin-tool.component.ts
@@ -1,22 +1,36 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Dish, DishTypes} from "../../dish-card/dish-card.component";
+import {DataService} from "../../data.service";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-admin-tool',
     templateUrl: './admin-tool.component.html',
     styleUrls: ["./admin-tool.component.css"]
 })
-export class AdminToolComponent implements OnInit {
+export class AdminToolComponent implements OnInit, OnDestroy {
 
     public dishTypes = Object.keys(DishTypes);
     public selectedDishTypes: DishTypes[] = [];
     public dishes: Dish[];
     public selectedPaymentType;
 
-    constructor() {
+    private subs = new Array<Subscription>();
+
+    constructor(
+        private dataService: DataService
+    ) {
     }
 
     ngOnInit(): void {
+        let sub = this.dataService.dishesObs.subscribe(dishes => {
+            this.dishes = dishes;
+        });
+        this.subs.push(sub);
+    }
+
+    ngOnDestroy() {
+        this.subs.forEach((sub) => sub.unsubscribe());
     }
 
     log(type) {
